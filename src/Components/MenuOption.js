37be804b66caadd44f110a66a84fd7be7faf7fce
lelehut022 <lelehut022@student.wethinkcode.co.za>
2023-08-15 21:58:26 +0200

    modified menuOption component

--- src/Components/MenuOption.js
+++ src/Components/MenuOption.js
@@ -1,9 +1,11 @@
 import {nav} from "../data.js"
+
 export default function MenuOption(){
     const info = nav.map(item => 
-        <div>
-            <div>
-                <img src={item.image} alt=""   />
+        <div key={item.id} className="hidden">
+            <div className="bg-red-400 ">
+                <p>{item.name}</p>
+                <img src={item.image} alt={item.name} />
             </div>
         </div>
         );
